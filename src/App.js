e02382cf836ae0b12ad58374ecb2464a939db79b
joaquinrapetti <j.rapetti92@gmail.com>
2022-05-10 11:57:08 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from "react";
-import Header from "./container/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Header />
-    </div>
-  );
-};
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from "./container";
+import { Navbar } from "./components";
+import "./App.css";
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
